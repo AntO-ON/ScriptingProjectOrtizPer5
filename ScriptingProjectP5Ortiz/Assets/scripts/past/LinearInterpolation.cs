d93aa8608ccd2d7bcd2af51b20a81e587e19ad52
AntO-ON <723529@student.dusd.net>
2024-02-14 12:46:18 -0800

    sav

---
+++ ScriptingProjectP5Ortiz/Assets/scripts/past/LinearInterpolation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+  // In this case, result = 4
+    float result = Math.Lerp (3f, 5f, 0.5f);
+  Vector3 from = new Vector (1f, 2f, 3f);
+  Vector3 from = new Vector (5f, 6f, 7f);
+  //Here result = (4, 5, 6)
+  Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Math.Lerp(light.intensity, 8f, 0.5f);
+    }
+    */
+}
